8ea0adef09d7aca8e0ea2ba1bd6e8a52c0bbd008
arman-ashrafian <ashy63@gmail.com>
2018-04-03 22:10:48 -0700

    initial

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", indexHandler)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "HELLO WOLRD")
+}
